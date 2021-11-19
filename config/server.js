ef59ce7563b6ec47f14f41cda77f8e704b6ad6b9
Jeremy Laviole <j.laviole@catie.fr>
2021-11-19 22:03:32 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c6d06745fcb6df9048bed9ac8762c0c5'),
+    },
+  },
+});
